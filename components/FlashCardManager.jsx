b16bc9a6f98192eda751730dbd5e7d7aede53f21
Santiago <santiago.dominguez@live.u-tad.com>
2025-11-09 19:04:38 +0100

    sizing

--- components/FlashCardManager.jsx
+++ components/FlashCardManager.jsx
@@ -5,15 +5,15 @@ const FlashCardManager = () => {
   const flashcards = [
     {
       question: "¿Para qué?",
-      answer: "Empezar con el propósito, la causa, el problema, el objetivo, o la creencia fundamental del equipo. Responde a la pregunta de por qué existe el producto y por qué debería importarle a alguien. Es el motor que inspira y conecta emocionalmente con el público."
+      answer: "Queremos que los grupos de amigos tengan una forma de divertirse haciendo tareas que fomenten el no uso de las pantallas."
     },
     {
       question: "¿Cómo?",
-      answer: "Procesos, estrategias o valores que hacen única la propuesta de valor y ayudan a diferenciar el producto de la competencia."
+      answer: "Hemos creado una app que te obliga ha hacer divertidas tareas para no quedar último y tener que realizar un reto propuesto por el resto del grupo."
     },
     {
       question: "¿Qué?",
-      answer: "Es el producto o servicio tangible que la empresa vende. Es lo que la mayoría de las empresas comunican primero, pero muchas veces es lo menos importante para inspirar lealtad. Incluir beneficios de la solución, cómo mejorará la situación actual. Incluir curva de valor frente a la competencia, identificar cual es vuestra funcionalidad clave y diferencial."
+      answer: "Contamos con una aplicación capaz de juntar tecnologí diversión y el no uso de las pantallas así como socializar y pasar momentos divertidos con los amigos."
     }
   ];
 
