db54212164313d1614747d32c7e367244b3dec2b
Santiago <santiago.dominguez@live.u-tad.com>
2025-11-09 19:11:59 +0100

    sizing

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,6 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-bg">
-        <div className="bg-blob bg-blob-1"></div>
-        <div className="bg-blob bg-blob-2"></div>
-      </div>
 
       <div className="header-content">
         <div className="header-logo">
